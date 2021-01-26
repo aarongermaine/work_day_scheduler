11b9a7cfede3664d2775ef46f704f46766e2d6dd
aarongermaine <vcnts0@gmail.com>
2021-01-26 16:25:38 -0700

    Basic framework , JS psuedocoding

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// i am using a planner to schedule my day
+
+//when i click start display said planner
+
+//display the current day at the top
+
+//when i scrolldown i can see all hours of the day
+
+//if the time displayed has already passed , change to a inactive color and a inactive display
+
+//if the time displayed is immediate have an active color displayed
+
+//if it is in the future have a future colored display
+
+//when i click a time block then i can enter into an event
+
+//when i click save on that time block
+
+//then the text entered is saved into that time block that was clicked in local storage
+
+// when i refresh all items for that day are saved
+
