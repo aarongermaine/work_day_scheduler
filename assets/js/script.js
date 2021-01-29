17507f6c5888f24794683a701c51e7a8e9cc760d
aarongermaine <vcnts0@gmail.com>
2021-01-28 21:50:26 -0700

    Restructuring of entire project to better format css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,17 @@
 $(document).ready(function() {
+
   
-var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-
-  for (var i = 0; i < hours.length; i++) {
-
-    var hourDiv = $("<div>");
-
-    hourDiv.addClass("time-block row hour");
-
-    hourDiv.attr("data-hour", hours[i]);
-
-    hourDiv.text(hours[i]);
-
-    $("#timeDiv").append(hourDiv);
-
-    console.log(hourDiv);
-    
-    function displayColors () {
-      
-      //if hour has passed display past class
-       if (moment(todaysDate).isBefore(hours)) {
-         hourDiv.addClass("past");
-       } else if (moment(todaysDate).isSame(hours)) {
-         hourDiv.addClass("present");
-       } else if (moment(todaysDate).isAfter(hours)) {
-         hourDiv.addClass("future");
-       }
-      //if hour is present display present class
-
-      //if hour is future display future class
-  
-    };
-
-    displayColors();
-
-  }
-
-
-
-
-
+//display the date (:
 var todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 var dateDisplay = $("#currentDay");
 
-
 $(dateDisplay).text(todaysDate);
+
+  
+
+
+
 });
 // i am using a planner to schedule my day
 
