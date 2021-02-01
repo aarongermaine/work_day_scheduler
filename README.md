f46bbe4c099dfa6e80df16e9fe8dd3d88d7b6af0
aarongermaine <vcnts0@gmail.com>
2021-01-31 18:59:37 -0700

    Final formatting touch ups

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# work_day_scheduler
-A calendar application that allows a user to save events for each hour of the day.
+
+
+!!! Work Day Scheduler !!!
+
+
+
+CONTENTS 
+----
+
+ * Introduction
+ * Requirements
+ * Installation
+ * Maintainers
+
+
+INTRODUCTION
+------------
+
+A calendar application that allows a user to save events for each hour of the work day.
+
+Image of site: assets\site-screenshot.png
+
+
+REQUIREMENTS
+------------
+
+1.) Any web browser of your choosing
+
+2.) The latest version of that browser
+
+3.) Copy the following link into browser:
+
+
+INSTALLATION
+------------
+
+No installation required.
+
+
+CONTRIBUTORS
+-----------
+
+ * Aaron Germaine - https://github.com/aarongermaine
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+//use moment to display the current date
+var todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
+var dateDisplay = $("#currentDay");
 
-$(".saveBtn").on("click", function() {
+$(dateDisplay).text(todaysDate);
+//saves the hour/description to local storage
+$(".saveBtn").on("click",function () {
    
     var descriptionValue = $(this).siblings(".description").val();
+    
+    
     var timeDisplay = $(this).parent().attr("id");
 
     
@@ -11,7 +18,7 @@ $(".saveBtn").on("click", function() {
   });
 
 
-
+//add classes based on the time of day
 function hourAssign() {
 
     var presentHour = moment().hour();
@@ -23,18 +30,27 @@ function hourAssign() {
         var timeBlock = parseInt($(this).attr("id").split("time")[1]);
 
         if (timeBlock < presentHour) {
+            
             $(this).addClass("past");
+            
             $(this).removeClass("future");
+            
             $(this).removeClass("present");
         } 
         else if (timeBlock === presentHour) {
+            
             $(this).removeClass("past");
+            
             $(this).addClass("present");
+           
             $(this).removeClass("future");
 
         } else {
+            
             $(this).removeClass("present");
+           
             $(this).removeClass("past");
+            
             $(this).addClass("future");
         }
 
@@ -44,6 +60,7 @@ function hourAssign() {
 
 hourAssign();
 
+//returns saved description/hour to page from local storage
 $("#time9 .description").val(localStorage.getItem("time9"));
 $("#time10 .description").val(localStorage.getItem("time10"));
 $("#time11 .description").val(localStorage.getItem("time11"));
@@ -54,33 +71,8 @@ $("#time15 .description").val(localStorage.getItem("time15"));
 $("#time16 .description").val(localStorage.getItem("time16"));
 $("#time17 .description").val(localStorage.getItem("time17"));
   
-var todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-var dateDisplay = $("#currentDay");
-
-$(dateDisplay).text(todaysDate);
 
 
 });
-// i am using a planner to schedule my day
 
-//when i click start display said planner
-
-//display the current day at the top
-
-//when i scrolldown i can see all hours of the day
-
-//if the time displayed has already passed , change to a inactive color and a inactive display
-
-//if the time displayed is immediate have an active color displayed
-
-//if it is in the future have a future colored display
-
-//when i click a time block then i can enter into an event
-
-//when i click save on that time block
-
-//then the text entered is saved into that time block that was clicked in local storage
-
-// when i refresh all items for that day are saved
 
